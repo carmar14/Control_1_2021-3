d4c8e8d2bb06de8b5fd94f6495be3d55118b3efc
Mario <carmar141414@hotmail.com>
2021-10-15 21:40:52 -0500

    Controladores PID
    Ejemplos controladores PI y PID

--- S12/control_pid.m
+++ S12/control_pid.m
@@ -59,13 +59,13 @@ legend('Compensado','Deseado')
 g=tf(4,[1 -6 0])
 
 g_d=tf(3,[1 1 3]);
-hold on
+figure
 step(g_d,'r')
 %legend('Sin compensar','Deseado')
 [P Z]=pzmap(g_d)
 
-Kp=3/4;
-Td=7/3;
+Kp=3.189/4;
+Td=7/(4*Kp); %7/3;
 g_c=tf([Kp*Td Kp],1);
 
 g_ol=g_c*g;
@@ -76,38 +76,54 @@ step(g_cl,'b');
 hold on
 step(g_d,'r')
 legend('Compensado','Deseado')
-% %----------control PD----------
-% 
-% 
+
+%----------control PID----------
+Kp=53/4;
+Ti=4*Kp/150;
+Td=(51+4)/(4*Kp);
+g_c=tf([Kp*Ti*Td Kp*Ti Kp],[Ti 0]);
+
+g_ol=g_c*g;
+g_cl=feedback(g_ol,1);
+[p z]=pzmap(g_cl)
+figure
+step(g_cl,'b');
+hold on
+step(g_d,'r')
+legend('Compensado','Deseado')
+% %----------control PID----------
+
+
+
 % %----------control algebraico----------
-% g=tf(4,[1 -6 0])
-% 
-% g_d=tf(3,[1 1 3]);
-% hold on
-% step(g_d,'r')
-% %legend('Sin compensar','Deseado')
-% [P Z]=pzmap(g_d)
-% 
-% den_a=conv([1 1 3],[1 5])
-% 
-% A=[1 0 -6 0;
-%    0 4 0 0;
-%    0 0 1 0;
-%    -6 0 0 4];
-% B=[6 15 1 8]';
-% x=inv(A)*B;
-% p0=x(1);
-% q0=x(2);
-% p1=x(3);
-% q1=x(4);
-% g_c=tf([q1 q0],[p1 p0]);
-% 
-% g_ol=g_c*g;
-% g_cl=feedback(g_ol,1);
-% [p z]=pzmap(g_cl)
-% figure
-% step(g_cl,'b');
-% hold on
-% step(g_d,'r')
-% legend('Compensado','Deseado')
-% %----------control algebraico----------
+g=tf(4,[1 -6 0])
+g_d=tf(3,[1 1 3]);
+%hold on
+figure
+step(g_d,'r')
+%legend('Sin compensar','Deseado')
+[P Z]=pzmap(g_d)
+
+den_a=conv([1 1 3],[1 5])
+
+A=[1 0 -6 0;
+   0 4 0 0;
+   0 0 1 0;
+   -6 0 0 4];
+B=[6 15 1 8]';
+x=inv(A)*B;
+p0=x(1);
+q0=x(2);
+p1=x(3);
+q1=x(4);
+g_c=tf([q1 q0],[p1 p0]);
+
+g_ol=g_c*g;
+g_cl=feedback(g_ol,1);
+[p z]=pzmap(g_cl)
+figure
+step(g_cl,'b');
+hold on
+step(g_d,'r')
+legend('Compensado','Deseado')
+%----------control algebraico----------
---
+++ S12/ejemplo_pid.m
@@ -0,0 +1,7 @@
+clc
+close all
+clear
+
+Kp=20;
+Ti=0.1;
+Td=1;
